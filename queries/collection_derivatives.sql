8aa62a94d6d0ae8f6614449f2fa5e092de321c11
Brenda Nguyen <bn16@rice.edu>
2020-01-28 16:13:00 -0600

    revise query for collection derivative

--- queries/collection_derivatives.sql
+++ queries/collection_derivatives.sql
@@ -2,23 +2,22 @@
 /* Note: Left join to persons because some of the submitters are not in the persons table */
 
 select
-	lm.portal_type as module_type,
-	m.moduleid as canonical_col_id,
 	m.name as canonical_title,
-	lm.moduleid as derivative_col_id,
+	m.moduleid as canonical_colid,
+	m.portal_type as canonical_type,
 	lm.name as derivative_title,
-	lm.created as created_at,
+	lm.moduleid as derivative_colid,
+	lm.portal_type as derivative_type,
+    lm.created as created_at,
 	lm.revised as last_revised,
 	p.firstname as first_name,
 	p.surname as last_name,
 	p.email as email
 from latest_modules as lm
-left join persons as p on p.personid = lm.submitter
-inner join modules as m on m.module_ident = lm.parent
-where lm.portal_type = 'Collection'
-and 'OpenStaxCollege' != any(lm.authors)
-and lm.parent in (
-	select module_ident
+join (
+	select module_ident, name, moduleid, portal_type
 	from modules
 	where 'OpenStaxCollege' = any(authors)
-	and portal_type = 'Collection');
+	and portal_type='Collection') m on m.module_ident = lm.parent
+left join persons as p on p.personid = lm.submitter
+where lm.portal_type = 'Collection';
